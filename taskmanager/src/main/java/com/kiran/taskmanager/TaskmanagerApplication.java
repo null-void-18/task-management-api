70297651a20827439e9d1763b13cc99b5c110f3d
Kiran Sekhar <kiransekhar18@gmail.com>
2025-09-06 20:49:51 +0530

    New taskmanager project initialization

---
+++ taskmanager/src/main/java/com/kiran/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
